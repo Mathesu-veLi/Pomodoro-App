3f2544865e818e6fd05f32dd078fac6c45b93799
Matheus Levi <matheuslevit@gmail.com>
2024-01-12 12:06:11 -0300

    creates a basic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <PomodoroTimer />
         </div>
     );
 }
